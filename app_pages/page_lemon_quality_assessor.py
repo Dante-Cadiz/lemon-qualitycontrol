3d9885965ae10d48f5a7a6f8b2ec7da2b0ce2d39
Dante-Cadiz <dantecadiz98@mail.com>
2022-11-10 13:55:55 +0000

    Create full app pages for lemon visualiser and quality assessor

--- app_pages/page_lemon_quality_assessor.py
+++ app_pages/page_lemon_quality_assessor.py
@@ -15,3 +15,35 @@ def page_lemon_quality_assessor_body():
         f"* The client is interested in accurately and instantly predicting from a given image "
         f"whether a lemon is of good or poor quality."
         )
+    
+    st.write(
+        f"* Link for the lemon images will go here "
+        f"Figure out how to handle the data cleaning step"
+        )
+
+    st.write("---")
+
+    images_buffer = st.file_uploader('Upload lemon images. You may select more than one.',
+                                        type='png',accept_multiple_files=True)
+   
+    if images_buffer is not None:
+        df_report = pd.DataFrame([])
+        for image in images_buffer:
+
+            img_pil = (Image.open(image))
+            st.info(f"Lemon Image: **{image.name}**")
+            img_array = np.array(img_pil)
+            st.image(img_pil, caption=f"Image Size: {img_array.shape[1]}px width x {img_array.shape[0]}px height")
+
+            version = 'v4'
+            resized_img = resize_input_image(img=img_pil, version=version)
+            pred_proba, pred_class = load_model_and_predict(resized_img, version=version)
+            plot_predictions_probabilities(pred_proba, pred_class)
+
+            df_report = df_report.append({"Name":image.name, 'Result': pred_class },
+                                        ignore_index=True)
+        
+        if not df_report.empty:
+            st.success("Analysis Report")
+            st.table(df_report)
+            st.markdown(download_dataframe_as_csv(df_report), unsafe_allow_html=True)
--- src/machine_learning/predictive_analysis.py
+++ src/machine_learning/predictive_analysis.py
@@ -14,7 +14,7 @@ def plot_predictions_probabilities(pred_proba, pred_class):
 
     prob_per_class= pd.DataFrame(
             data=[0,0],
-            index={'Bad Quality': 0, 'Good Quality': 1}.keys(),
+            index={'bad_quality': 0, 'good_quality': 1}.keys(),
             columns=['Probability']
         )
     prob_per_class.loc[pred_class] = pred_proba
@@ -39,6 +39,7 @@ def resize_input_image(img, version):
     """
     image_shape = load_pkl_file(file_path=f"outputs/{version}/image_shape.pkl")
     img_resized = img.resize((image_shape[1], image_shape[0]), Image.ANTIALIAS)
+    # need to get image shape correct
     my_image = np.expand_dims(img_resized, axis=0)/255
 
     return my_image
@@ -52,6 +53,8 @@ def load_model_and_predict(my_image, version):
     model = load_model(f"outputs/{version}/lemon_quality_model.h5")
 
     pred_proba = model.predict(my_image)[0,0]
+    # this is raising a value error currently because the image input array is of a different format
+    # to what the model expects
 
     target_map = {v: k for k, v in {'Bad Quality': 0, 'Good Quality': 1}.items()}
     pred_class =  target_map[pred_proba > 0.5]  
