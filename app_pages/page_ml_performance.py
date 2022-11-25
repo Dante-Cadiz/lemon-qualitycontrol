ee1138f5e6cbb64990b2fab33dbbae74adca2cb2
Dante-Cadiz <dantecadiz98@mail.com>
2022-11-25 16:19:08 +0000

    Format ML performance file to be PEP8 compliant

--- app_pages/page_ml_performance.py
+++ app_pages/page_ml_performance.py
@@ -10,14 +10,15 @@ def page_ml_performance_metrics():
 
     st.write("### Train, Validation and Test Set: Labels Frequencies")
 
-    labels_distribution = plt.imread(f"outputs/{version}/labels_distribution.png")
-    st.image(labels_distribution, caption='Labels Distribution on Train, Validation and Test Sets')
+    labels_distribution = plt.imread(
+                           f"outputs/{version}/labels_distribution.png")
+    st.image(labels_distribution,
+             caption='Labels Distribution on Train, Validation and Test Sets')
     st.write("---")
-
 
     st.write("### Model History")
     col1, col2 = st.beta_columns(2)
-    with col1: 
+    with col1:
         model_acc = plt.imread(f"outputs/{version}/model_training_acc.png")
         st.image(model_acc, caption='Model Training Accuracy')
     with col2:
@@ -26,14 +27,15 @@ def page_ml_performance_metrics():
     st.write("---")
 
     st.write("### Generalised Performance on Test Set")
-    st.dataframe(pd.DataFrame(load_test_evaluation(version), index=['Loss', 'Accuracy']))
+    st.dataframe(pd.DataFrame(load_test_evaluation(version),
+                              index=['Loss', 'Accuracy']))
     confusion_matrix = plt.imread(f"outputs/{version}/confusion_matrix.png")
     st.image(confusion_matrix, caption="Confusion Matrix", width=500)
 
     st.write(f"* The overall accuracy of 0.9646, as well as a recall score "
-             f"on the negative outcome of a bad quality lemon of 0.98 indicate that "
-             f"this machine learning model is successful in detrmining the quality of a "
-             f"lemon, and conforms with the success metrics as defined in the ML " 
-             f"business requirements of an F1 score of 0.95 or higher and a recall on "
-             f"bad quality lemons of 0.98 or higher"
-    )
+             f"on the negative outcome of a bad quality lemon of 0.98 indicate"
+             f" that this machine learning model is successful in determining "
+             f" the quality of a lemon, and conforms with the success metrics "
+             f"as defined in the ML business requirements of an F1 score of "
+             f"0.95 or higher and a recall on bad quality lemons of 0.98 or "
+             f"higher.")
